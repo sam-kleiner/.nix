f3940843e8bd5781a4e159f355bfa87eca412132
Sam Kleiner <sam-kleiner@users.noreply.github.com>
2022-10-08 12:49:14 -0400

    nouvue

--- machines/common/nvidia.nix
+++ machines/common/nvidia.nix
@@ -4,6 +4,8 @@
   # NVIDIA drivers are unfree.
   nixpkgs.config.allowUnfree = true;
 
+  #services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.videoDrivers = [ "nouveau" ];
   hardware.opengl.enable = true;
 
   # Optionally, you may need to select the appropriate driver version for your specific GPU.
