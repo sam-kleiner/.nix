42f34af5372bc4e625cf2a6ff783d9394ca210e2
Sam Kleiner <sam-kleiner@users.noreply.github.com>
2022-11-30 23:44:07 -0500

    add xone

--- machines/common/gaming.nix
+++ machines/common/gaming.nix
@@ -2,4 +2,11 @@
 
 {
   programs.steam.enable = true;
+  # boot.extraModprobeConfig = '' options bluetooth disable_ertm=1 '';
+  hardware.xone.enable = true;
+  # hardware.xpadneo.enable = true;
+  # services.hardware.xow.enable = true;
+  environment.systemPackages = with pkgs; [
+    linuxKernel.packages.linux_zen.xone
+  ];
 }
